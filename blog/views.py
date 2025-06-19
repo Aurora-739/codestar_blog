c96d19b2b0a4344da8e7f1a9840a662458b5b225
Aurora-739 <yfifej9y@students.codeinstitute.net>
2025-06-19 09:57:52 +0100

    change to blog views

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from blog import models
 from django.views import generic
-from .models import Post
+from models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1)
     template_name = "post_list.html"
